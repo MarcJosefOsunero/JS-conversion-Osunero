a228be900b6298f99e13b3d9b7b40c51e3053a7e
unknown <osunero.marc@gmail.com>
2023-03-30 17:29:37 +0800

    second commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,5 @@
 function litersConversion(value) {
-    document.getElementById("Centiliter").innerHTML=value*100;
-    document.getElementById("Mililiter").innerHTML=value*1000;
+    document.getElementById("Centiliter").innerHTML= value*100;
+    document.getElementById("Mililiter").innerHTML= value*1000;
+    document.getElementById("Ounces").innerHTML= value*33.814;
   }
